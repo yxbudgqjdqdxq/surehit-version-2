796882893a2d851ad8f549d14170ae45d2ea03de
yxbudgqjdqdxq <93042363+yxbudgqjdqdxq@users.noreply.github.com>
2025-09-25 10:48:16 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 
-// pages/app.js
+// pages/_app.js
 import Head from "next/head";
 import AnimatedBackground from "../components/AnimatedBackground";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  // favicon: small white heart SVG as data URL
+export default function MyApp({ Component, pageProps }) {
+  // small white-heart favicon (data URL)
   const faviconSvg = encodeURIComponent(`
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
       <path fill='white' d='M12 21s-7.33-4.94-9.2-8.02C.8 9.9 3.2 6.5 6.2 6.5c1.7 0 2.68 1.1 3.3 2.05.33.52.62 1.02 1.5 1.02.88 0 1.17-.5 1.5-1.02.62-.95 1.6-2.05 3.3-2.05 3 0 5.4 3.4 3.4 6.48C19.33 16.06 12 21 12 21z'/>
